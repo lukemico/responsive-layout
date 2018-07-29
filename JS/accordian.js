5a3cbc9f511433965a43b96c870adebe09a4f138
Luke Mico <lukemico@me.com>
2018-07-30 01:43:03 +1000

    Layout + Accordian updates

--- JS/accordian.js
+++ JS/accordian.js
@@ -1,32 +1,13 @@
- $(".accordion").on("click", ".accordion-header", function() {
+$(document).ready(function() {
+	$('.accordion-content-1').show();
+});
+
+
+$(".accordion").on("click", ".accordion-header", function() {
  	$(this).toggleClass("active").next().slideToggle();
- });
+});
 
 
-
-  $(document).ready(function($) {
-    $('#accordion').find('.accordion-toggle').click(function(){
-
-      //Expand or collapse this panel
-      $(this).next().slideToggle('fast');
-
-      //Hide the other panels
-      $(".accordion-content").not($(this).next()).slideUp('slow');
-
-    });
-  });
-
-/*
-
-(function($) {
-    
-  var allPanels = $('.accordion-2 > dd').hide();
-    
-  $('.accordion-2 > dt > a').click(function() {
-    allPanels.slideUp();
-    $(this).parent().next().slideDown();
-    return false;
-  });
-
-})(jQuery);
-*/
+1
+2
+3
