3be2a556fc76df6736334f4e3a67c19864cab937
Luke Mico <lukemico@me.com>
2018-07-30 10:59:11 +1000

    update readme

--- JS/accordian.js
+++ JS/accordian.js
@@ -1,21 +1,23 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
 	
-	// Open the first accordion
-	var firstAccordion = acc[0];
-	var firstPanel = firstAccordion.nextElementSibling;
-	firstAccordion.classList.add("active");
-	firstPanel.style.maxHeight = firstPanel.scrollHeight + "px";
+  // Open the first accordion
+  var firstAccordion = acc[0];
+  var firstPanel = firstAccordion.nextElementSibling;
+  firstAccordion.classList.add("active");
+  firstPanel.style.maxHeight = firstPanel.scrollHeight + "px";
 
-	// Add onclick listener to every accordion element
-	for (i = 0; i < acc.length; i++) {
-    	acc[i].onclick = function() {
-		// For toggling purposes detect if the clicked section is already "active"
-		var isActive = this.classList.contains("active");
+  // Add onclick listener to every accordion element
+  for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+      // For toggling purposes detect if the clicked section is already "active"
+      var isActive = this.classList.contains("active");
 
-		// Close all accordions
-		var allAccordions = document.getElementsByClassName("accordion");
-		for (j = 0; j < allAccordions.length; j++) {
+      // Close all accordions
+      var allAccordions = document.getElementsByClassName("accordion");
+      for (j = 0; j < allAccordions.length; j++) {
         // Remove active class from section header
         allAccordions[j].classList.remove("active");
 
@@ -23,52 +25,52 @@ var i;
         var panel = allAccordions[j].nextElementSibling;
         var maxHeightValue = getStyle(panel, "maxHeight");
       
-			if (maxHeightValue !== "0px") {
-			panel.style.maxHeight = null;
-        	}
-    	}
-		
-		// Toggle the clicked section using a ternary operator
-		isActive ? this.classList.remove("active") : this.classList.add("active");
+      if (maxHeightValue !== "0px") {
+          panel.style.maxHeight = null;
+        }
+      }
 
-		// Toggle the panel element
-		var panel = this.nextElementSibling;
-		var maxHeightValue = getStyle(panel, "maxHeight");
+      // Toggle the clicked section using a ternary operator
+      isActive ? this.classList.remove("active") : this.classList.add("active");
+
+      // Toggle the panel element
+      var panel = this.nextElementSibling;
+      var maxHeightValue = getStyle(panel, "maxHeight");
       
-		if (maxHeightValue !== "0px") {
-        	panel.style.maxHeight = null;
-    	} else {
-        	panel.style.maxHeight = panel.scrollHeight + "px";
-    	}
+      if (maxHeightValue !== "0px") {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
     };
-}
+  }
   
-	// Cross-browser way to get the computed height of a certain element. 
-	function getStyle(el, styleProp) {
-	var value, defaultView = (el.ownerDocument || document).defaultView;
-  
-	if (defaultView && defaultView.getComputedStyle) {
-    	// sanitize property name to css notation
-		// (hypen separated words eg. font-Size)
-		styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
-		return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
-	} else if (el.currentStyle) { // IE
-    	// sanitize property name to camelCase
-		styleProp = styleProp.replace(/\-(\w)/g, function(str, letter) {
-		return letter.toUpperCase();
+  // Cross-browser way to get the computed height of a certain element. Credit to @CMS on StackOverflow (http://stackoverflow.com/a/2531934/7926565)
+  function getStyle(el, styleProp) {
+  var value, defaultView = (el.ownerDocument || document).defaultView;
+  // W3C standard way:
+  if (defaultView && defaultView.getComputedStyle) {
+    // sanitize property name to css notation
+    // (hypen separated words eg. font-Size)
+    styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
+    return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+  } else if (el.currentStyle) { // IE
+    // sanitize property name to camelCase
+    styleProp = styleProp.replace(/\-(\w)/g, function(str, letter) {
+      return letter.toUpperCase();
     });
     value = el.currentStyle[styleProp];
     // convert other units to pixels on IE
     if (/^\d+(em|pt|%|ex)?$/i.test(value)) { 
-    	return (function(value) {
-        	var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
-			el.runtimeStyle.left = el.currentStyle.left;
-			el.style.left = value || 0;
-			value = el.style.pixelLeft + "px";
-			el.style.left = oldLeft;
-			el.runtimeStyle.left = oldRsLeft;
-		return value;
-    	})(value);
+      return (function(value) {
+        var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
+        el.runtimeStyle.left = el.currentStyle.left;
+        el.style.left = value || 0;
+        value = el.style.pixelLeft + "px";
+        el.style.left = oldLeft;
+        el.runtimeStyle.left = oldRsLeft;
+        return value;
+      })(value);
     }
     return value;
   }
--- readme.md
+++ readme.md
@@ -1,8 +1,10 @@
-Introduction
----
-Thanks for taking the time to complete this frontend technical assessment. We will be focusing on code quality (reusability, readability, maintainability, etc.) and your eye for detail. Feel free to add libraries and edit the HTML files as required. Bonus points if SASS/LESS is used. 
+###Responsive Layout
 
-*Estimated Time: 45m - 1h* 
+
+==================== Overview ==============================
+
+##Task
+
 
 Exercise 1
 ---
@@ -20,6 +22,7 @@ exercise1.html
 * Mobile banner - http://via.placeholder.com/600x600
 * Content images - http://via.placeholder.com/400x300
 
+
 Exercise 2
 ---
 Convert the HTML to an accordion.
@@ -35,3 +38,85 @@ exercise2.html
 ###### Bonus points
 * Improve the user experience with meaningful animations/transitions.
 * Make it prettier.# responsive-layout
+
+
+
+##Installing
+To view the files locally using Python SimpleServer -
+
+* To start a HTTP server on port 8000 (default port), type in Command Line:
+
+	python -m SimpleHTTPServer [port]
+
+* This shows the files and directories which are in the current working directory.
+
+* In your terminal, 'cd' or change directory into whichever directory you wish to have accessible via browsers and HTTP.
+
+	cd /var/www/
+
+	$ python -m SimpleHTTPServer
+
+* After you hit enter, you should see the following message:
+
+	Serving HTTP on 0.0.0.0 port 8000 ...
+
+* Open your preferred browser and put in any of the following addresses:
+
+	(http://your_ip_address:8000)
+
+	(http://127.0.0.1:8000)
+
+
+##Features
+
+Exercise 1 
+1. Responsive Bootstrap layout using media queries for Desktop and Mobile versions.
+2. Desktop version renders 1440x650 placeholder image. Mobile version renders 600x600 placeholder image. 
+
+
+Exercise. 2
+
+1. 1 accordion opens at a time when clicked.
+2. The first accordion is open on document load.
+3. If the open or active accordion is clicked, the panel is closed.
+
+
+## Next Steps 
+Exercise 1 
+1. Implement SASS/Less CSS styling for improved global styling across a website or application.
+2. Implement image assets that can be called/ used within CSS file.
+
+
+Exercise. 2
+
+1. Implement SASS/Less CSS styling for improved global styling across a website or application.
+2. Workshop transitions and provide suggestions to further the UX/UI experience for a user. 
+	
+ 
+## Languages
+* HTML - markup
+* CSS - used to CSS Grid + styling
+* JavaScript - handle the data and its outputs
+
+
+
+==================== Languages ==============================
+
+* HTML - markup
+* JavaScript - handle the data and its outputs
+* CSS - Used to override some defaults
+
+
+==================== Libraries ==============================
+
+* n/a
+
+
+
+##Versioning
+* I used Git for versioning. For the versions available, see the tags on this repository.
+
+##Author/s
+	Luke Mico
+
+
